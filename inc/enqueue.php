209c78d6a9ccc3a85e57d42504dd8fad6b0cb396
Matthew Campbell <m.campbell92@gmail.com>
2025-03-14 11:40:33 +0100

    enqueue styles

---
+++ inc/enqueue.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined('ABSPATH'))
+    exit;
+
+
+function register_styles()
+{
+
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('eo-main', get_template_directory_uri() . '/style.css', array(), $version, 'all');
+}
+add_action('wp_enqueue_scripts', 'register_styles');
+
+function register_scripts()
+{
+    // gets loaded with wp_footer() in 
+    // don't forget to change 'filename'
+    wp_enqueue_script('eo-main', get_template_directory_uri() . '/assets/js/filename.js', array(), '1.0');
+}
+add_action('wp_enqueue_scripts', 'register_scripts');
