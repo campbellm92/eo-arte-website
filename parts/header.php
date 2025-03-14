7f7dd02bacded9809ef99f7a45f35c9cec6f0112
Matthew Campbell <m.campbell92@gmail.com>
2025-03-14 11:41:23 +0100

    load header

---
+++ parts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    wp_head();
+    ?>
+</head>
+
+<body>
+
+    <header>
+        <a href="" class="">Title</a>
+        <nav>
+            <?php ?>
+        </nav>
+    </header>
