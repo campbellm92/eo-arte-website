453247e73584b3d7f8548a0ab71db79cacb1948b
Matthew Campbell <m.campbell92@gmail.com>
2025-03-18 16:14:45 +0100

    add section idea for srolling effect

--- front-page.php
+++ front-page.php
@@ -4,13 +4,22 @@ get_template_part('parts/header');
 
 <main>
 
-    <section class="bg-blue h-screen">
+    <section class="bg-blue h-screen" id="hero-section">
         <div class="flex flex-col justify-center items-center w-full h-full">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-eo.webp" alt="Spazio EO logo"
                 class="h-auto w-auto p-3">
         </div>
 
     </section>
+
+    <section class="bg-gray h-screen">
+
+    </section>
+
+    <section class="bg-gray h-screen">
+
+    </section>
+
 
 
 </main>
