f4e841ba76a1d172ba447e03864bb0482147b3a4
Matthew Campbell <m.campbell92@gmail.com>
2025-03-18 12:34:34 +0100

    constract header

--- parts/header.php
+++ parts/header.php
@@ -10,10 +10,21 @@
 </head>
 
 <body>
-
-    <header>
-        <a href="" class=""></a>
-        <nav>
-            <?php ?>
+    <header class="bg-gray">
+        <nav class="flex justify-between items-center w-full p-4">
+            <a href="/" class="text-blue font-semibold">spazio eo</a>
+            <ul class="flex justify-end space-x-4">
+                <?php
+                $pages = array("/", "/about", "/eventi", "/corsi", "/contattateci");
+                $page_names = array("home", "about", "eventi", "corsi", "contattateci");
+                $style = "text-gray font-semibold bg-blue px-3 rounded-sm";
+                foreach ($page_names as $index => $page_name) {
+                    $href = $pages[$index];
+                    echo "<li class='$style'><a href=$href>$page_name</a></li>";
+                }
+                ?>
+            </ul>
         </nav>
+
+
     </header>
