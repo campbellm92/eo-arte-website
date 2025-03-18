b38e89ed8f685175fe17037a1921463e854cb042
Matthew Campbell <m.campbell92@gmail.com>
2025-03-18 12:33:58 +0100

    initial frontpage styling

--- front-page.php
+++ front-page.php
@@ -2,71 +2,15 @@
 get_template_part('parts/header');
 ?>
 
-<main class="flex flex-col justify-center items-center h-screen">
+<main>
 
-    <div class="flex flex-col gap-4">
-        <!-- Primary + Secondary Buttons -->
-        <div>
-            <p>Primary + Secondary</p>
-            <?php
-            echo primary_button('Button', '#', '');
-            echo secondary_button('Button', '#', '');
-            ?>
+    <section class="bg-blue h-screen">
+        <div class="flex flex-col justify-center items-center w-full h-full">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-eo.webp" alt="Spazio EO logo"
+                class="h-auto w-auto p-3">
         </div>
 
-        <!-- Primary Outline Buttons -->
-        <div>
-            <p>Primary Outline</p>
-            <div>1: <?php
-            echo primary_outline_button('Button', '#', '');
-
-            ?></div>
-            <div>2: <?php
-
-            echo primary_outline_button_alt('Button', '#', '');
-            ?></div>
-
-        </div>
-
-        <!-- Secondary Outline Buttons -->
-        <div>
-            <p>Secondary Outline</p>
-            <div>1:
-                <?php
-                echo secondary_outline_button('Button', '#', '');
-
-                ?>
-            </div>
-            <div>2:
-                <?php
-
-                echo secondary_outline_button_alt('Button', '#', '');
-                ?>
-            </div>
-
-        </div>
-
-        <!-- Shadow Buttons -->
-        <div>
-            <p>Shadow Buttons</p>
-            <div class="mb-4">
-                <?php
-                echo primary_shadow_button('Button', '#', '');
-
-                ?>
-            </div>
-
-            <div>
-                <?php
-
-                echo secondary_shadow_button('Button', '#', '');
-                ?>
-            </div>
-
-        </div>
-
-    </div>
-    </div>
+    </section>
 
 
 </main>
