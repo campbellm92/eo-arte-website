24ca08147fd8e0a795fcc802ebd4d41861018cd4
Matthew Campbell <m.campbell92@gmail.com>
2025-03-20 12:36:57 +0100

    fix styles on h1, p, remove div container around img

--- front-page.php
+++ front-page.php
@@ -4,15 +4,36 @@ get_template_part('parts/header');
 
 <main>
 
-    <section class="bg-blue h-screen" id="hero-section">
-        <div class="flex flex-col justify-center items-center w-full h-full">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-eo.webp" alt="Spazio EO logo"
-                class="h-auto w-auto p-3">
+    <section class="bg-blue min-h-screen flex justify-center items-center" id="hero-section">
+
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-eo.webp" alt="Spazio EO logo"
+            class="p-6">
+
+    </section>
+
+    <section
+        class="min-h-screen bg-[url(/wp-content/themes/eo-main/assets/images/eo-arte-outside.webp)] bg-no-repeat bg-center bg-cover lg:bg-fixed flex flex-col justify-center">
+        <!-- get_template_directory_uri not working here..? -->
+
+        <div class="w-5/6 max-w-3xl bg-gray m-10 p-4">
+            <h1 class="text-red font-extrabold ">ABOUT</h1>
+            <p class="text-red font-semibold ">It's 3am, time to create some chaos
+                commence
+                midnight zoomies yet
+                i
+                is not fat, i is fluffy waffles terrorize
+                the hundred-and-twenty-pound rottweiler and steal his bed, not sorry. Claws in the eye of the beholder
+                go
+                crazy with excitement when plates are clanked together signalling the arrival of cat food my water bowl
+                is
+                clean and freshly replenished, so i'll drink from the toilet for the dog smells bad so sun bathe, so
+                intrigued by the shower.</p>
         </div>
 
     </section>
 
-    <section class="bg-gray h-screen">
+
+
 
     </section>
 
