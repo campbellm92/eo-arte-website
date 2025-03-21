fa506a36724e638cf8b8c9aaa6caf9b61833b36b
Matthew Campbell <m.campbell92@gmail.com>
2025-03-21 12:07:59 +0100

    require event post type

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
+if (!defined('ABSPATH'))
+    exit;
 
 //functions
 require_once 'inc/enqueue.php';
 
 //components
 require_once 'components/buttons.php';
+
+// custom post types
+require_once 'inc/custom-post-types/event.php';
