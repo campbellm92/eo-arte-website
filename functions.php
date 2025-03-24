9e0fe5298bc191bd1415df413251654ab2e2d495
Matthew Campbell <m.campbell92@gmail.com>
2025-03-24 18:42:01 +0100

    add workshop to posttypes

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ require_once 'components/buttons.php';
 
 // custom post types
 require_once 'inc/custom-post-types/event.php';
+require_once 'inc/custom-post-types/workshop.php';
 
 // theme support
-
 require_once 'inc/theme-support.php';
