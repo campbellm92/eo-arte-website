7a3c28dec837500ee3da418e34c1c25506e7f5ee
Matthew Campbell <m.campbell92@gmail.com>
2025-03-26 10:40:22 +0100

    move navbar scroll change script to separate js folder

--- parts/header.php
+++ parts/header.php
@@ -29,29 +29,4 @@
                 ?>
             </ul>
         </nav>
-
-        <!-- script for changing navbar color for different sections -->
-        <script>
-            document.addEventListener("DOMContentLoaded", function () {
-                const mainNav = document.getElementById("main-nav");
-                const siteTitle = document.getElementById("site-title")
-                const navLinks = document.querySelectorAll(".nav-links");
-                const heroSection = document.getElementById("hero-section");
-
-                window.addEventListener("scroll", () => {
-                    if (window.scrollY > heroSection?.offsetHeight / 1.1) {
-                        // the default or 'scrolled' styles
-                        mainNav.classList.add("main-nav-scrolled");
-                        siteTitle.classList.add("site-title-scrolled")
-                        navLinks.forEach(link => link.classList.add("links-default"));
-                    } else {
-                        // the styles at the hero section
-                        mainNav.classList.remove("main-nav-scrolled");
-                        siteTitle.classList.remove("site-title-scrolled")
-                        navLinks.forEach(link => link.classList.remove("links-default"));
-                    }
-                })
-            })
-        </script>
-
     </header>
