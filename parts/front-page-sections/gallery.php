f2133b2bb6b09c2f02612d8eb3bba2d4f78595f8
Matthew Campbell <m.campbell92@gmail.com>
2025-03-26 10:40:42 +0100

    add test gallery

---
+++ parts/front-page-sections/gallery.php
@@ -0,0 +1,25 @@
+<!-- gallery section -->
+
+<section class="min-h-screen bg-gray masonry-grid">
+
+    <!-- add images for testing -->
+    <!-- will probably need to write a custom "gallery" type -->
+
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/1.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-up animate">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/2.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-up animate">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/3.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-u animate">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/4.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-up animate">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/5.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-up animate">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/6.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-up animate">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/7.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-up animate">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/8.webp" alt="Spazio EO logo"
+        class="p-6 slide-in-up animate">
+
+</section>
