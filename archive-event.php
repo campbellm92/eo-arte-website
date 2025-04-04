b1afbcd1f8bb73453eac0b3282d8e46e8f35274d
Matthew Campbell <m.campbell92@gmail.com>
2025-04-04 12:23:26 +0200

    experiment with different heights and widths for responsivity

--- archive-event.php
+++ archive-event.php
@@ -23,19 +23,21 @@ $events = new WP_Query($args);
 ?>
 
 
-<main class="bg-blue pt-[5rem] px-10">
+<main class="bg-blue pt-[5rem]">
 
+    <div class="px-5">
+        <h1 class="text-gray mb-5">EVENTI</h1>
 
-    <h1 class="text-gray mb-10">EVENTI</h1>
+        <div class="mb-10">
+            <?php echo (new Button('ATTUALI E PROSSIMI', '#'))->type('neutral')->variant('outline')->addClass('event-toggle-btn show-present-events')->render(); ?>
+            <?php echo (new Button('ARCHIVIO', '#'))->type('neutral')->variant('outline')->addClass('event-toggle-btn show-past-events')->render(); ?>
+        </div>
 
-    <div class="mb-10">
-        <?php echo (new Button('ATTUALI E PROSSIMI', '#'))->type('neutral')->variant('outline')->addClass('event-toggle-btn show-present-events')->render(); ?>
-        <?php echo (new Button('ARCHIVIO', '#'))->type('neutral')->variant('outline')->addClass('event-toggle-btn show-past-events')->render(); ?>
     </div>
 
 
     <!-- Current and upcoming -->
-    <section class="min-h-screen">
+    <section class="min-h-screen px-5">
         <?php if ($events->have_posts()):
             while ($events->have_posts()):
                 $events->the_post(); ?>
@@ -45,14 +47,16 @@ $events = new WP_Query($args);
                 $event_date_tba = get_post_meta(get_the_ID(), '_event_date_tba', true); ?>
 
 
-                <div class="flex flex-col md:flex-row w-full gap-4 text-gray hover:bg-red cursor-pointer border-b-4 py-6">
+                <div
+                    class="flex flex-col items-center md:flex-row w-full gap-4 lg:h-64 text-gray hover:bg-red cursor-pointer border-b-4 py-6">
                     <?php if (has_post_thumbnail()): ?>
-                        <div class="rounded-xs max-w-xs">
+                        <div
+                            class="rounded-xs w-full md:w-[12.5rem] md:h-[10rem] lg:w-[18.75rem] lg:h-[12.5rem] overflow-hidden shrink-0">
                             <?php the_post_thumbnail() ?>
                         <?php endif ?>
                     </div>
                     <div class="flex flex-col md:flex-row justify-between items-center gap-4 mb-6">
-                        <h2 class="px-4 mt-6 shrink-0 max-w-prose"><?php the_title(); ?></h2>
+                        <h2 class="px-4 mt-6 max-w-[20ch] leading-tight break-words shrink-0"><?php the_title(); ?></h2>
                         <p class="sm:px-10">
                             <?php if ($event_date_tba == 1): ?>
                                 Da annunciare
