a03f449b893d9c28679060e9fa91516e86a42f74
Matthew Campbell <m.campbell92@gmail.com>
2025-04-07 16:24:42 +0200

    add helpers for formatting date and time

---
+++ inc/date-time-formatters.php
@@ -0,0 +1,39 @@
+<?php
+
+if (!defined('ABSPATH'))
+    exit;
+
+function convert_date_format($date_string)
+{
+
+    if (!$date_string)
+        return '';
+
+    $date = new DateTime($date_string);
+
+    $formatter = new IntlDateFormatter('it_IT', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Rome', IntlDateFormatter::GREGORIAN, "EEEE d MMMM");
+
+    return $formatter->format($date);
+
+}
+
+function convert_time_format($time_string)
+{
+
+    if (!$time_string)
+        return '';
+
+    $time = new DateTime($time_string);
+
+    $formatter = new IntlDateFormatter(
+        'it_IT',
+        IntlDateFormatter::NONE,
+        IntlDateFormatter::SHORT,
+        'Europe/Rome',
+        IntlDateFormatter::GREGORIAN,
+        "HH:mm"
+    );
+
+    return $formatter->format($time);
+
+}
