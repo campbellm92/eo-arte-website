557e31cce9a11e65577c0befba3f5797ba15c89d
Matthew Campbell <m.campbell92@gmail.com>
2025-04-08 16:48:58 +0200

    add page

---
+++ single-event.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('ABSPATH'))
+    exit;
+?>
+
+<?php get_template_part('parts/header'); ?>
+
+<main>
+
+    <section class="h-screen">hey</section>
+    <section class="h-screen">hey</section>
+    <section class="h-screen">hey</section>
+    <section class="h-screen">hey</section>
+    <section class="h-screen">hey</section>
+    <section class="h-screen">hey</section>
+
+</main>
+
+
+
+<?php get_template_part('parts/footer'); ?>
