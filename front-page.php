bcdee6732723a70489c054212e064ff087f611e7
Matthew Campbell <m.campbell92@gmail.com>
2025-04-08 16:47:16 +0200

    update section imports after rename

--- front-page.php
+++ front-page.php
@@ -8,12 +8,12 @@ get_template_part('parts/header');
 ?>
 
 <main>
-    <?php get_template_part('parts/front-page-sections/hero') ?>
-    <?php get_template_part('parts/front-page-sections/about') ?>
-    <?php get_template_part('parts/front-page-sections/events') ?>
-    <?php get_template_part('parts/front-page-sections/photo-bg-one') ?>
-    <?php get_template_part('parts/front-page-sections/courses') ?>
-    <?php get_template_part('parts/front-page-sections/gallery') ?>
+    <?php get_template_part('parts/front-page-sections/hero-section') ?>
+    <?php get_template_part('parts/front-page-sections/about-section') ?>
+    <?php get_template_part('parts/front-page-sections/events-section') ?>
+    <?php get_template_part('parts/front-page-sections/photo-bg-one-section') ?>
+    <?php get_template_part('parts/front-page-sections/workshops-section') ?>
+    <?php get_template_part('parts/front-page-sections/gallery-section') ?>
 </main>
 
 <?php
