15deabd90722771fb112a5c53019a37ca3eca99a
Matthew Campbell <m.campbell92@gmail.com>
2025-04-09 16:50:00 +0200

    being construction of single event page

--- single-event.php
+++ single-event.php
@@ -6,13 +6,64 @@ if (!defined('ABSPATH'))
 <?php get_template_part('parts/header'); ?>
 
 <main>
+    <section class="bg-gray min-h-screen">
+        <?php if (have_posts()):
+            while (have_posts()):
+                the_post(); ?>
+                <?php
+                $event_date_from = get_post_meta(get_the_ID(), '_event_date_from', true);
+                $event_date_to = get_post_meta(get_the_ID(), '_event_date_to', true);
+                $event_date_tba = get_post_meta(get_the_ID(), '_event_date_tba', true); ?>
+                <div class="min-h-full h-screen">
+                    <?php if (has_post_thumbnail()): ?>
+                        <div class="flex items-end w-full h-full bg-no-repeat bg-center bg-cover mb-10"
+                            style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'full')); ?>');">
 
-    <section class="h-screen">hey</section>
-    <section class="h-screen">hey</section>
-    <section class="h-screen">hey</section>
-    <section class="h-screen">hey</section>
-    <section class="h-screen">hey</section>
-    <section class="h-screen">hey</section>
+                            <?php
+                            $date_string = convert_to_day_and_short_month($event_date_from);
+                            $parts = explode(' ', $date_string);
+                            ?>
+                            <div class="bg-gray max-w-3xl h-auto m-10 rounded-xs">
+                                <div class="flex flex-col text-center p-2">
+                                    <span class="text-5xl font-bold text-red pb-0"><?php echo $parts[0] ?></span>
+                                    <span class="text-5xl font-bold text-red"><?php echo $parts[1] ?></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 gap-4 min-h-screen">
+                            <div class="col-span-8 pl-4">
+                                <h2><?php the_title() ?></h2>
+                                <p><?php the_content() ?></p>
+                            </div>
+
+                            <div class="col-span-4 col-start-10">
+                                <div class="text-red text-md font-bold">
+                                    <?php if ($event_date_tba == 1): ?>
+                                        Da annunciare
+                                    <?php elseif ($event_date_from && $event_date_to): ?>
+                                        <?php echo esc_html(convert_date_format($event_date_from)) . " → " . esc_html(convert_date_format($event_date_to)); ?>
+                                    <?php elseif ($event_date_from):
+                                        echo esc_html(convert_date_format($event_date_from))
+                                            ?>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php endif ?>
+
+                </div>
+
+
+
+
+
+
+
+            <?php endwhile; endif; ?>
+    </section>
+
 
 </main>
 
