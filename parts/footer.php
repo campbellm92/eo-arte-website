98a7928cd78320e7265399b9edc17c73dac11202
Matthew Campbell <m.campbell92@gmail.com>
2025-04-14 19:07:54 +0200

    initial footer markup

--- parts/footer.php
+++ parts/footer.php
@@ -1,4 +1,16 @@
-<footer></footer>
+<footer class="flex flex-col items-center md:flex-row md:justify-between bg-blue w-full mt-auto p-4">
+
+    <div class="flex justify-center md:text-left mb-6 md:mb-0">
+        <a href="/" class="text-gray font-semibold" id="site-title">spazio eo</a>
+    </div>
+
+    <div class="w-64 h-64 ">
+        <?php echo get_svg_markup('mappa-eo') ?>
+    </div>
+
+
+
+</footer>
 
 <?php
 wp_footer();
