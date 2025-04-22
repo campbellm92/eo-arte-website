fda82edf97ae7a580df8d7da6d5ff6c6a00a0383
Matthew Campbell <m.campbell92@gmail.com>
2025-04-22 15:15:55 +0200

    remove nonfunctional ispage logic for now

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -15,10 +15,10 @@ add_action('wp_enqueue_scripts', 'register_styles');
 
 function register_scripts()
 {
-    if (is_page('eventi', 'workshop')) {
-        wp_enqueue_script('toggle-upcoming-archive', get_template_directory_uri() . '/assets/js/toggleUpcomingAndArchive.js', [], null, true);
+    // if (is_page('eventi', 'workshop')) {
+    wp_enqueue_script('toggle-upcoming-archive', get_template_directory_uri() . '/assets/js/toggleUpcomingAndArchive.js', [], null, true);
 
-    }
+    // }
 
 }
 add_action('wp_enqueue_scripts', 'register_scripts');
