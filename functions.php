99c27fd8e002a713c4294ea11ee3e05b6830a5d0
Matthew Campbell <m.campbell92@gmail.com>
2025-04-29 16:21:37 +0200

    import custom post type

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ require_once 'components/buttons.php';
 // custom post types
 require_once 'inc/custom-post-types/event.php';
 require_once 'inc/custom-post-types/workshop.php';
+require_once 'inc/custom-post-types/artist.php';
 
 // theme support
 require_once 'inc/theme-support.php';
@@ -19,3 +20,4 @@ require_once 'inc/theme-support.php';
 require_once 'inc/img-to-webp.php';
 require_once 'inc/date-time-formatters.php';
 require_once 'inc/get-svg-markup.php';
+// require_once 'inc/contact-form-logic.php';
