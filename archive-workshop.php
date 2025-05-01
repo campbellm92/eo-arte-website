063762136e134a5b7bd0f342ee881a975010f673
Matthew Campbell <m.campbell92@gmail.com>
2025-05-01 11:37:24 +0200

    refactor workshop cards

--- archive-workshop.php
+++ archive-workshop.php
@@ -40,48 +40,42 @@ $workshops = new WP_Query($args);
 
     <!-- Current and upcoming -->
     <section class="min-h-screen px-5">
-        <?php if ($workshops->have_posts()):
-            while ($workshops->have_posts()):
-                $workshops->the_post(); ?>
-                <?php
-                $workshop_date_from = get_post_meta(get_the_ID(), '_workshop_date_from', true);
-                $workshop_date_to = get_post_meta(get_the_ID(), '_workshop_date_to', true);
-                $workshop_date_tba = get_post_meta(get_the_ID(), '_workshop_date_tba', true); ?>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <?php if ($workshops->have_posts()):
+                while ($workshops->have_posts()):
+                    $workshops->the_post(); ?>
+                    <?php
+                    $workshop_date_from = get_post_meta(get_the_ID(), '_workshop_date_from', true);
+                    $workshop_date_to = get_post_meta(get_the_ID(), '_workshop_date_to', true);
+                    $workshop_date_tba = get_post_meta(get_the_ID(), '_workshop_date_tba', true); ?>
 
-                <!-- Section container  -->
-                <div class="flex flex-col items-center md:flex-row w-full gap-4 lg:h-64 text-gray hover:bg-red cursor-pointer border-b-4"
-                    onclick="location.href='<?php echo get_the_permalink(); ?>'">
-                    <?php if (has_post_thumbnail()): ?>
-                        <div class="rounded-xs w-full md:w-[12.5rem] lg:w-[18.75rem] h-full overflow-hidden shrink-0">
-                            <?php the_post_thumbnail('full', ['class' => 'object-cover w-full h-full']) ?>
+                    <div class="cursor-pointer" onclick="location.href='<?php echo get_the_permalink(); ?>'">
+                        <!-- Content Container  -->
+                        <div
+                            class="flex flex-col justify-between h-full gap-4 p-6 border-8 border-light-gray hover:bg-red text-gray">
+                            <!-- Content -->
+                            <h2 class="break-words"><?php the_title(); ?></h2>
+                            <p class="text-xl">
+                                <?php if ($workshop_date_tba == 1): ?>
+                                    Da annunciare
+                                <?php elseif ($workshop_date_from && $workshop_date_to): ?>
+                                    <?php echo esc_html(convert_date_format($workshop_date_from)) . " → " . esc_html(convert_date_format($workshop_date_to)); ?>
+                                <?php elseif ($workshop_date_from):
+                                    echo esc_html(convert_date_format($workshop_date_from))
+                                        ?>
+                                <?php endif; ?>
+                            </p>
+                            <p class="text-xl text-left line-clamp-3 overflow-hidden">
+                                <?php echo get_the_content() ?>
+                            </p>
                         </div>
-                    <?php endif ?>
-
-                    <!-- Content Container -->
-                    <div class="flex flex-col md:flex-row justify-between items-center w-full h-full gap-4 mb-6 py-6">
-                        <!-- Content -->
-                        <h2 class="px-4 mt-6 md:w-1/3 leading-tight break-words shrink-0"><?php the_title(); ?></h2>
-                        <p class="sm:px-10 md:w-1/3 text-center text-xl md:text-left">
-                            <?php if ($workshop_date_tba == 1): ?>
-                                Da annunciare
-                            <?php elseif ($workshop_date_from && $workshop_date_to): ?>
-                                <?php echo esc_html(convert_date_format($workshop_date_from)) . " → " . esc_html(convert_date_format($workshop_date_to)); ?>
-                            <?php elseif ($workshop_date_from):
-                                echo esc_html(convert_date_format($workshop_date_from))
-                                    ?>
-                            <?php endif; ?>
-                        </p>
-                        <p class="px-4 text-center text-xl md:text-left md:w-1/3 line-clamp-3 overflow-hidden">
-                            <?php echo get_the_content() ?>
-                        </p>
                     </div>
-                </div>
-            <?php endwhile; endif; ?>
-
+                <?php endwhile; endif; ?>
+        </div>
     </section>
 
 
-
+    <!-- 
     <div class="w-full rounded-xs overflow-hidden bg-gray max-w-xs border-8 border-red">
         <?php if (has_post_thumbnail()): ?>
 
@@ -108,7 +102,7 @@ $workshops = new WP_Query($args);
         <div class="flex justify-center w-full px-3.5 pt-2 pb-3.5">
             <?php echo (new Button('ENQUIRE', '#'))->type('neutral')->variant('filled')->addClass('w-full')->render(); ?>
         </div>
-    </div>
+    </div> -->
 
 
 
