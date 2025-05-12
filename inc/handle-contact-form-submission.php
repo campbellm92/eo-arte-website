fae32f540b09abdba25b576f4dbfba445f02dd1c
Matthew Campbell <m.campbell92@gmail.com>
2025-05-12 19:52:07 +0200

    rename and rewrite of wp form logic

---
+++ inc/handle-contact-form-submission.php
@@ -0,0 +1,22 @@
+<?php
+
+function handle_contact_form_submission()
+{
+    $name = sanitize_text_field($_POST['name']);
+    $email = sanitize_email($_POST['email']);
+    $message = sanitize_textarea_field($_POST['message']);
+
+    $adminEmail = get_option('admin_email');
+
+    wp_mail($adminEmail, sprintf("New message from %s email: %s", $name, $email), $message);
+
+    error_log("Sending contact form email to: " . $adminEmail);
+
+    wp_redirect(home_url('/'));
+    exit;
+}
+add_action('admin_post_nopriv_submit_contact_form', 'handle_contact_form_submission');
+add_action('admin_post_submit_contact_form', 'handle_contact_form_submission');
+
+
+
