1e382809a2d96c4b1761246dc82a9b8ae8badc06
Matthew Campbell <m.campbell92@gmail.com>
2025-05-16 11:28:29 +0200

    fix animation flicker on redirect to home

--- assets/js/loadingScreen.js
+++ assets/js/loadingScreen.js
@@ -13,11 +13,12 @@ window.addEventListener("DOMContentLoaded", function () {
 
   if (loadingScreen) {
     document.body.style.overflow = "hidden";
-    setTimeout(() => {
-      loadingScreen.style.display = "flex";
-      loadingScreen.style.opacity = 0;
-      loadingScreen.style.transition = "opacity 0.5s ease";
 
+    loadingScreen.style.display = "flex";
+    loadingScreen.style.opacity = 1;
+    loadingScreen.style.transition = "opacity 0.5s ease";
+    setTimeout(() => {
+      loadingScreen.style.opacity = 0;
       setTimeout(() => {
         loadingScreen.style.display = "none";
         document.body.style.overflow = "auto";
