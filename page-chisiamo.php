7e82444552ed2f297e49eb3defec61653c18645c
Matthew Campbell <m.campbell92@gmail.com>
2025-11-08 19:42:21 +0100

    add layout to about page

---
+++ page-chisiamo.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!defined('ABSPATH'))
+    exit;
+?>
+
+
+<?php
+get_template_part('parts/header');
+?>
+
+<main class="min-h-screen pt-[9rem]">
+    <section class="p-9 max-w-10/12">
+        <h1 class="pb-5">chi siamo*</h1>
+        <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
+            is
+            not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
+            sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
+            signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
+            the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+    </section>
+    <section class="p-9 max-w-10/12">
+        <h1 class="pb-5">servizi di eo*</h1>
+        <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
+            is
+            not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
+            sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
+            signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
+            the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+    </section>
+
+</main>
+
+
+<?php get_template_part('parts/footer'); ?>
