b71ddf85ee0f8e0145d7ab4691d211cdeae2601e
Matthew Campbell <m.campbell92@gmail.com>
2025-11-13 18:28:02 +0100

    remove picture backgrounds

--- front-page.php
+++ front-page.php
@@ -9,11 +9,9 @@ get_template_part('parts/header');
 
 <main>
     <?php get_template_part('parts/front-page-sections/hero-section') ?>
-    <?php get_template_part('parts/front-page-sections/about-section') ?>
     <?php get_template_part('parts/front-page-sections/events-section') ?>
-    <?php get_template_part('parts/front-page-sections/photo-bg-one-section') ?>
+    <?php get_template_part('parts/front-page-sections/about-section') ?>
     <?php get_template_part('parts/front-page-sections/workshops-section') ?>
-    <?php get_template_part('parts/front-page-sections/gallery-section') ?>
 </main>
 
 <?php
