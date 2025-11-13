bbfc33f34e6de9745fd93cb9c49cc548347fd380
Matthew Campbell <m.campbell92@gmail.com>
2025-11-13 18:29:22 +0100

    add new collective page

---
+++ page-colletivo.php
@@ -0,0 +1,42 @@
+<?php
+
+if (!defined('ABSPATH'))
+    exit;
+?>
+
+
+<?php
+get_template_part('parts/header');
+?>
+
+<main class="min-h-screen pt-[9rem]">
+    <section class="p-9 max-w-10/12">
+        <h1 class="pb-5">colletivo*</h1>
+        <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
+            is
+            not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
+            sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
+            signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
+            the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+    </section>
+    <section class="p-9 max-w-10/12">
+        <h1 class="pb-5">colletivo*</h1>
+        <div class="grid-cols-3">
+
+            <div class="flex flex-row">
+                <div class="rounded-full overflow-hidden max-w-48 max-h-48">
+                    <img src="" alt="">
+                </div>
+                <div class="flex flex-col">
+                    <p>Nome Cognome</p>
+                    <p>professione</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+</main>
+
+
+<?php get_template_part('parts/footer'); ?>
