89d78391f3e8a3241bd8a87cbccfc72f9cffb59e
Matthew Campbell <m.campbell92@gmail.com>
2025-11-29 17:12:06 +0100

    remove workshops section

--- front-page.php
+++ front-page.php
@@ -11,7 +11,6 @@ get_template_part('parts/header');
     <?php get_template_part('parts/front-page-sections/hero-section') ?>
     <?php get_template_part('parts/front-page-sections/events-section') ?>
     <?php get_template_part('parts/front-page-sections/about-section') ?>
-    <?php get_template_part('parts/front-page-sections/workshops-section') ?>
 </main>
 
 <?php
