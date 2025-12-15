d6d05edeb7b557f3f45f99685c476b3f7c134ce8
Matthew Campbell <m.campbell92@gmail.com>
2025-12-15 15:20:32 +0100

    update titles

--- page-chisiamo.php
+++ page-chisiamo.php
@@ -11,7 +11,7 @@ get_template_part('parts/header');
 
 <main class="min-h-screen w-full md:max-w-10/12 pt-[9rem] text-center md:text-left">
     <section class="p-9">
-        <h1 class="pb-5">chi siamo*</h1>
+        <h1 class="pb-5">chi siamo</h1>
         <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
             is
             not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
@@ -20,7 +20,7 @@ get_template_part('parts/header');
             the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
     </section>
     <section class="p-9">
-        <h1 class="pb-5">servizi di eo*</h1>
+        <h1 class="pb-5">servizi di eo</h1>
         <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
             is
             not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
