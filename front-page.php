1bab7eabad1f6f7719626476e7ef2070110adbf2
Matthew Campbell <m.campbell92@gmail.com>
2025-12-15 15:15:19 +0100

    update imports

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@ get_template_part('parts/header');
 
 <main>
     <?php get_template_part('parts/front-page-sections/hero-section') ?>
-    <?php get_template_part('parts/front-page-sections/events-section') ?>
+    <?php get_template_part('parts/front-page-sections/in-evidenza-section') ?>
     <?php get_template_part('parts/front-page-sections/about-section') ?>
 </main>
 
