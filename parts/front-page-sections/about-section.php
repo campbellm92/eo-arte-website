2dec74214d331dd62d0cc62076b3bbd4bc47c88a
Matthew Campbell <m.campbell92@gmail.com>
2025-12-15 15:23:28 +0100

    update titles

--- parts/front-page-sections/about-section.php
+++ parts/front-page-sections/about-section.php
@@ -1,8 +1,8 @@
 <!-- about section -->
-<section class="flex flex-col p-8 mt-10 min-h-screen max-w-10/12">
+<section class="flex flex-col p-8 mt-10 min-h-screen w-full md:max-w-10/12 text-center md:text-left">
     <div class="p-5">
-        <h1 class="pb-5">about*</h1>
-        <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
+        <h1 class="pb-5 text-black">about</h1>
+        <p class="text-dark-gray">It's 3am, time to create some chaos commence midnight zoomies yet i
             is
             not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
             sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
@@ -10,7 +10,7 @@
             the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
     </div>
     <div class="p-5">
-        <h1 class="pb-5">servizi di eo*</h1>
+        <h1 class="pb-5">servizi di eo</h1>
         <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
             is
             not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
