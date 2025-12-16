8ae5d1d1156fa38ddcb4232395b90e9380340733
Matthew Campbell <m.campbell92@gmail.com>
2025-12-16 12:18:05 +0100

    update imports

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ if (!defined('ABSPATH'))
 
 //functions and styles
 require_once 'inc/enqueue.php';
+require_once 'inc/cleanup.php';
+require_once 'inc/gallery.php';
 
 //components
 require_once 'components/buttons.php';
@@ -17,7 +19,7 @@ require_once 'inc/custom-post-types/artist.php';
 require_once 'inc/theme-support.php';
 
 //handlers
-require_once 'inc/img-to-webp.php';
+require_once 'inc/convert-img-to-webp-on-media-upload.php';
 require_once 'inc/date-time-formatters.php';
 require_once 'inc/get-svg-markup.php';
 require_once 'inc/handle-contact-form-submission.php';
