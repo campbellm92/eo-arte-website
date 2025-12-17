4397c3cfa7891fba8cfe08fd1feb0c332409b871
Matthew Campbell <m.campbell92@gmail.com>
2025-12-17 12:04:05 +0100

    remove pr-0 after md+ for content

--- parts/single-event-or-workshop-page-sections/content.php
+++ parts/single-event-or-workshop-page-sections/content.php
@@ -4,11 +4,12 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
+
 <div class="col-span-8">
     <h2 class="text-dark-gray pb-4">
         <?php the_title(); ?>
     </h2>
-    <div class="text-dark-gray pr-4 md:pr-0">
+    <div class="text-dark-gray pr-4">
         <?php the_content(); ?>
     </div>
 
