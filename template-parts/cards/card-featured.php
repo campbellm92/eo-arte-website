75761c9a0b55611cd7ac986db9105e7422c319f5
Matthew Campbell <m.campbell92@gmail.com>
2025-12-17 12:05:16 +0100

    add fallback styles in case of no img, fix issue - ui displaying shortcode

--- template-parts/cards/card-featured.php
+++ template-parts/cards/card-featured.php
@@ -1,18 +1,30 @@
-<div class="relative group flex-shrink-0 w-11/12 sm:w-full md:w-2/3 lg:w-1/2 
-            h-[32rem] md:h-[40rem] rounded-xs overflow-hidden snap-start">
+<?php
 
-    <?php if (has_post_thumbnail()): ?>
+$has_thumb = has_post_thumbnail();
+
+?>
+
+<div class="relative group flex-shrink-0 w-11/12 sm:w-full md:w-2/3 lg:w-1/2 
+            h-[32rem] md:h-[40rem] rounded-xs overflow-hidden snap-start 
+            <?php echo $has_thumb ? '' : 'bg-white text-black'; ?>">
+
+    <?php if ($has_thumb): ?>
         <?php the_post_thumbnail('large', [
             'class' => 'w-full h-full object-cover transition-transform duration-500 group-hover:scale-105'
         ]); ?>
     <?php endif; ?>
-    <div
-        class="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center text-gray p-6 text-center">
+    <div class="
+        absolute inset-0 flex flex-col justify-center items-center p-6 text-center transition-opacity duration-300
+        <?php if ($has_thumb): ?>
+            bg-black/60 text-gray opacity-0 group-hover:opacity-100
+        <?php else: ?>
+            bg-gray text-dark-gray opacity-100
+        <?php endif; ?>
+    ">
         <h2 class="text-2xl font-bold mb-2"><?php the_title(); ?></h2>
         <p class="text-sm font-semibold mb-4">
             <?php
             $type = get_post_type();
-
             if ($type === 'event') {
                 get_template_part('template-parts/dates/date-event');
             } elseif ($type === 'workshop') {
@@ -21,7 +33,7 @@
             ?>
         </p>
         <div class="line-clamp-3 mb-4 px-4">
-            <?php echo wp_kses_post(get_the_content()); ?>
+            <?php echo wp_kses_post(get_the_excerpt()); ?>
         </div>
         <?php echo (new Button('SCOPRI DI PIÙ', get_permalink()))
             ->type('primary-solid')
