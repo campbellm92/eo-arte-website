69042844feac191b1feb7db7c761748e11e61bb8
Matthew Campbell <m.campbell92@gmail.com>
2025-12-17 12:00:26 +0100

    update imports

--- functions.php
+++ functions.php
@@ -3,12 +3,9 @@ if (!defined('ABSPATH'))
     exit;
 
 //functions and styles
-require_once 'inc/enqueue.php';
-require_once 'inc/cleanup.php';
-require_once 'inc/gallery.php';
-
-//components
-require_once 'components/buttons.php';
+require_once 'inc/assets/enqueue.php';
+require_once 'inc/core/cleanup.php';
+require_once 'inc/media/gallery.php';
 
 // custom post types
 require_once 'inc/custom-post-types/event.php';
@@ -16,10 +13,13 @@ require_once 'inc/custom-post-types/workshop.php';
 require_once 'inc/custom-post-types/artist.php';
 
 // theme support
-require_once 'inc/theme-support.php';
+require_once 'inc/core/theme-support.php';
 
-//handlers
-require_once 'inc/convert-img-to-webp-on-media-upload.php';
-require_once 'inc/date-time-formatters.php';
-require_once 'inc/get-svg-markup.php';
-require_once 'inc/handle-contact-form-submission.php';
+//handlers + utils
+require_once 'inc/utils/convert-img-to-webp-on-media-upload.php';
+require_once 'inc/utils/date-time-formatters.php';
+require_once 'inc/utils/get-svg-markup.php';
+require_once 'inc/handlers/handle-contact-form-submission.php';
+
+//components
+require_once 'components/buttons.php';
