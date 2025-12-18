5d5684e92fe787a9a0f649f6a15602a7c216c486
Matthew Campbell <m.campbell92@gmail.com>
2025-12-18 15:44:08 +0100

    change how hero displays days for workshops

--- template-parts/hero-dates/hero-date-workshop.php
+++ template-parts/hero-dates/hero-date-workshop.php
@@ -1,11 +1,14 @@
 <?php
-$date_from = get_post_meta(get_the_ID(), '_workshop_date_from', true);
+$days = get_post_meta(get_the_ID(), '_workshop_days', true);
 $date_tba = get_post_meta(get_the_ID(), '_workshop_date_tba', true);
 
-if ($date_tba == 1 || empty($date_from)) {
-    echo '<span class="text-3xl font-bold text-red">Workshop</span>';
+if ($date_tba === 1 || empty($days)) {
+    echo '<span class="text-3xl font-bold text-red">Da annunciare</span>';
     return;
 }
 
-$date_string = convert_to_day_and_short_month($date_from);
-echo '<span class="text-4xl font-bold text-red">' . esc_html($date_string) . '</span>';
+$days_label = is_array($days)
+    ? implode(', ', $days)
+    : $days;
+
+echo '<span class="text-4xl font-bold text-red">' . esc_html($days_label) . '</span>';
