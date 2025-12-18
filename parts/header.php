1f94fd3187a922d9bcdf0c74af4d317fa3dc187d
Matthew Campbell <m.campbell92@gmail.com>
2025-12-18 17:53:54 +0100

    add transparent bg to nav, align dots for mob

--- parts/header.php
+++ parts/header.php
@@ -22,19 +22,20 @@
 
     <header>
         <!-- main nav -->
-        <nav class="flex justify-between items-center max-w-screen w-full p-4 fixed top-0 z-50" id="main-nav">
+        <nav class="flex justify-between items-center max-w-screen w-full bg-gray/45 p-4 fixed top-0 z-50"
+            id="main-nav">
             <a href="/" class="p-3" id="site-title"><img
                     src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo-eo-arte.svg"
                     alt="EO Arte logo" class="w-20"></a>
             <div class="flex items-center space-x-4">
                 <!-- menu toggle icon -->
-                <div class="w-7 cursor-pointer text-6xl mx-20" id="toggle-icon">...</div>
+                <div class="w-7 cursor-pointer text-5xl mx-10 md:mx-20 text-blue" id="toggle-icon">•••</div>
             </div>
             <div class="absolute top-full left-0 w-full p-6 z-50 hidden bg-gray h-screen" id="menu">
                 <ul class="flex flex-col space-y-8">
                     <?php
-                    $pages = array("/chisiamo", "/in-evidenza", "/colletivo", "/contattateci");
-                    $page_names = array("CHI SIAMO", "IN EVIDENZA", "COLLETIVO", "CONTATTATECI");
+                    $pages = array("/chisiamo", "/in-evidenza", "/colletivo", "/contattaci");
+                    $page_names = array("CHI SIAMO", "IN EVIDENZA", "COLLETIVO", "CONTATTACI");
                     $style =
                         "text-3xl px-3 hover:text-red transition duration-600 ease-in-out";
                     foreach ($page_names as $index => $page_name) {
