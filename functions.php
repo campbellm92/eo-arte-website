385ba0ec95d72a924b79597939edd8876b9c6aea
Matthew Campbell <m.campbell92@gmail.com>
2025-12-18 15:40:25 +0100

    clean up

--- functions.php
+++ functions.php
@@ -2,24 +2,33 @@
 if (!defined('ABSPATH'))
     exit;
 
-//functions and styles
+// require helpers, custom post types, filters, settings from inc/
+
+// WP admin
+require_once 'inc/admin/admin-sidebar.php';
+
+// assets
 require_once 'inc/assets/enqueue.php';
+
+//core
 require_once 'inc/core/cleanup.php';
-require_once 'inc/media/gallery.php';
+require_once 'inc/core/theme-support.php';
 
 // custom post types
 require_once 'inc/custom-post-types/event.php';
 require_once 'inc/custom-post-types/workshop.php';
 require_once 'inc/custom-post-types/artist.php';
 
-// theme support
-require_once 'inc/core/theme-support.php';
+// handlers
+require_once 'inc/handlers/handle-contact-form-submission.php';
 
-//handlers + utils
+// media
+require_once 'inc/media/gallery.php';
+
+// utils
 require_once 'inc/utils/convert-img-to-webp-on-media-upload.php';
 require_once 'inc/utils/date-time-formatters.php';
 require_once 'inc/utils/get-svg-markup.php';
-require_once 'inc/handlers/handle-contact-form-submission.php';
 
-//components
+// components
 require_once 'components/buttons.php';
