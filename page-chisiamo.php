13032da3724016486fc23d83282bb5a46ec5ec0f
Matthew Campbell <m.campbell92@gmail.com>
2025-12-18 17:52:31 +0100

    change layout

--- page-chisiamo.php
+++ page-chisiamo.php
@@ -9,26 +9,48 @@ if (!defined('ABSPATH'))
 get_template_part('parts/header');
 ?>
 
-<main class="min-h-screen w-full md:max-w-10/12 pt-[9rem] text-center md:text-left">
-    <section class="p-9">
-        <h1 class="pb-5">chi siamo</h1>
-        <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
-            is
-            not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
-            sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
-            signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
-            the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
-    </section>
-    <section class="p-9">
-        <h1 class="pb-5">servizi di eo</h1>
-        <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
-            is
-            not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
-            sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
-            signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
-            the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
-    </section>
-
+<main class="min-h-screen w-full pt-[9rem]">
+    <div class="mx-auto w-full max-w-6xl px-6">
+        <section class="grid grid-cols-12 gap-6 mb-24">
+            <div class="col-span-12 md:col-span-7">
+                <h1 class="pb-5">CHI SIAMO</h1>
+                <p class="max-w-prose">It's 3am, time to create some chaos commence midnight zoomies yet i
+                    is
+                    not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
+                    not
+                    sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
+                    signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink
+                    from
+                    the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+            </div>
+        </section>
+        <section class="grid grid-cols-12 gap-6 mb-24">
+            <div class="col-span-12 md:col-span-9 md:col-start-6 md:text-right">
+                <h1 class="pb-5">SERVIZI</h1>
+                <p class="max-w-prose md:ml-auto">It's 3am, time to create some chaos commence midnight zoomies yet i
+                    is
+                    not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
+                    not
+                    sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
+                    signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink
+                    from
+                    the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+            </div>
+        </section>
+        <section class="grid grid-cols-12 gap-6 mb-24">
+            <div class="col-span-12 md:col-span-7">
+                <h1 class="pb-5">LA STORIA DI EO BAUSSANO</h1>
+                <p class="max-w-prose">It's 3am, time to create some chaos commence midnight zoomies yet i
+                    is
+                    not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
+                    not
+                    sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
+                    signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink
+                    from
+                    the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+            </div>
+        </section>
+    </div>
 </main>
 
 
