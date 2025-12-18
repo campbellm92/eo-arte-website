b151f14f323b7bd2ebdd147905c0baadb9f6c066
Matthew Campbell <m.campbell92@gmail.com>
2025-12-18 17:54:33 +0100

    change layout;

--- parts/front-page-sections/about-section.php
+++ parts/front-page-sections/about-section.php
@@ -1,17 +1,17 @@
 <!-- about section -->
-<section class="flex flex-col p-8 mt-10 min-h-screen w-full md:max-w-10/12 text-center md:text-left">
-    <div class="p-5">
-        <h1 class="pb-5 text-black">about</h1>
-        <p class="text-dark-gray">It's 3am, time to create some chaos commence midnight zoomies yet i
+<section class="flex flex-col gap-4 p-8 mt-10 min-h-screen w-full">
+    <div class="md:p-5">
+        <h1 class="pb-5">CHI SIAMO</h1>
+        <p class="md:w-[65%]">It's 3am, time to create some chaos commence midnight zoomies yet i
             is
             not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
             sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
             signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
             the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
     </div>
-    <div class="p-5">
-        <h1 class="pb-5">servizi di eo</h1>
-        <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
+    <div class="flex flex-col md:items-end md:p-5">
+        <h1 class="pb-5">SERVIZI</h1>
+        <p class="md:w-[65%]">It's 3am, time to create some chaos commence midnight zoomies yet i
             is
             not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
             sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
