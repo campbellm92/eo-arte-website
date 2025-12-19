be812a21bf213ca37038e37f8bf4b91771c39c9f
Matthew Campbell <m.campbell92@gmail.com>
2025-12-19 17:23:01 +0100

    change: alt text for photo

--- parts/front-page-sections/hero-section.php
+++ parts/front-page-sections/hero-section.php
@@ -1,5 +1,4 @@
-<!-- section with EO icon (hero) -->
 <section class="min-h-screen flex justify-center items-center" id="hero-section">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/eo-old-entry.webp" alt="Spazio EO logo"
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/eo-old-entry.webp" alt="Foto dello spazio di EO"
         class="h-screen w-full object-cover">
 </section>
