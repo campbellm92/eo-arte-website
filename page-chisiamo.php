1ef67ffbb2e27466a3ee26104a08c1b94b84c208
Matthew Campbell <m.campbell92@gmail.com>
2025-12-19 17:20:47 +0100

    adjust text widths

--- page-chisiamo.php
+++ page-chisiamo.php
@@ -9,12 +9,12 @@ if (!defined('ABSPATH'))
 get_template_part('parts/header');
 ?>
 
-<main class="min-h-screen w-full pt-[9rem]">
-    <div class="mx-auto w-full max-w-6xl px-6">
+<main class="min-h-screen w-full pt-40">
+    <div class="mx-auto w-full px-10">
         <section class="grid grid-cols-12 gap-6 mb-24">
             <div class="col-span-12 md:col-span-7">
                 <h1 class="pb-5">CHI SIAMO</h1>
-                <p class="max-w-prose">It's 3am, time to create some chaos commence midnight zoomies yet i
+                <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
                     is
                     not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
                     not
@@ -27,7 +27,7 @@ get_template_part('parts/header');
         <section class="grid grid-cols-12 gap-6 mb-24">
             <div class="col-span-12 md:col-span-9 md:col-start-6 md:text-right">
                 <h1 class="pb-5">SERVIZI</h1>
-                <p class="max-w-prose md:ml-auto">It's 3am, time to create some chaos commence midnight zoomies yet i
+                <p class="md:ml-auto">It's 3am, time to create some chaos commence midnight zoomies yet i
                     is
                     not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
                     not
@@ -40,7 +40,7 @@ get_template_part('parts/header');
         <section class="grid grid-cols-12 gap-6 mb-24">
             <div class="col-span-12 md:col-span-7">
                 <h1 class="pb-5">LA STORIA DI EO BAUSSANO</h1>
-                <p class="max-w-prose">It's 3am, time to create some chaos commence midnight zoomies yet i
+                <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
                     is
                     not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
                     not
