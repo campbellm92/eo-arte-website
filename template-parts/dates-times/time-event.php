1fec85faa5d81e26e07c39cd0e858157667d3104
Matthew Campbell <m.campbell92@gmail.com>
2025-12-22 09:02:41 +0100

    change: times to have prepositions oinstead of arrows

--- template-parts/dates-times/time-event.php
+++ template-parts/dates-times/time-event.php
@@ -2,13 +2,15 @@
 
 $event_time_from = get_post_meta(get_the_ID(), '_event_time_from', true);
 $event_time_to = get_post_meta(get_the_ID(), '_event_time_to', true);
+$event_date_tba = get_post_meta(get_the_ID(), '_event_date_tba', true);
 
 if ($event_time_from && !$event_time_to) {
     echo esc_html(convert_time_format($event_time_from));
 } elseif ($event_time_from && $event_time_to) {
-    echo esc_html(convert_time_format($event_time_from))
-        . " → "
+    echo 'dalle '
+        . esc_html(convert_time_format($event_time_from))
+        . " alle "
         . esc_html(convert_time_format($event_time_to));
-} else {
+} elseif (!$event_date_tba && !$event_time_from) {
     echo esc_html__('Ore da annunciare');
 }
