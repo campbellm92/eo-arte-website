24db726caf5fa73ecd1a8dd259af4c3203eeccd5
Matthew Campbell <m.campbell92@gmail.com>
2025-12-22 10:05:50 +0100

    add high fetch priority to hero img

--- parts/front-page-sections/hero-section.php
+++ parts/front-page-sections/hero-section.php
@@ -1,4 +1,4 @@
 <section class="min-h-screen flex justify-center items-center" id="hero-section">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/eo-old-entry.webp" alt="Foto dello spazio di EO"
-        class="h-screen w-full object-cover">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero.webp" alt="Foto dello spazio di EO"
+        class="h-screen w-full object-cover" fetchpriority="high">
 </section>
