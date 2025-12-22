6111a1c893f044cb2ddb4807b2ca4ad9a6b3c919
Matthew Campbell <m.campbell92@gmail.com>
2025-12-22 16:45:07 +0100

    refactor to consume layout template

--- page-chisiamo.php
+++ page-chisiamo.php
@@ -1,57 +1,32 @@
 <?php
-
 if (!defined('ABSPATH'))
     exit;
-?>
 
-
-<?php
 get_template_part('parts/header');
-?>
-
-<main class="min-h-screen w-full pt-40">
-    <div class="mx-auto w-full px-10">
-        <section class="grid grid-cols-12 gap-6 mb-24">
-            <div class="col-span-12 md:col-span-7">
-                <h1 class="pb-5">CHI SIAMO</h1>
-                <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
-                    is
-                    not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
-                    not
-                    sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
-                    signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink
-                    from
-                    the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
-            </div>
-        </section>
-        <section class="grid grid-cols-12 gap-6 mb-24">
-            <div class="col-span-12 md:col-span-9 md:col-start-6 md:text-right">
-                <h1 class="pb-5">SERVIZI</h1>
-                <p class="md:ml-auto">It's 3am, time to create some chaos commence midnight zoomies yet i
-                    is
-                    not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
-                    not
-                    sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
-                    signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink
-                    from
-                    the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
-            </div>
-        </section>
-        <section class="grid grid-cols-12 gap-6 mb-24">
-            <div class="col-span-12 md:col-span-7">
-                <h1 class="pb-5">LA STORIA DI EO BAUSSANO</h1>
-                <p class="">It's 3am, time to create some chaos commence midnight zoomies yet i
-                    is
-                    not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed,
-                    not
-                    sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
-                    signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink
-                    from
-                    the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
-            </div>
-        </section>
-    </div>
-</main>
 
 
-<?php get_template_part('parts/footer'); ?>
+$sections = [
+    [
+        'title' => 'Chi Siamo',
+        'content' => get_field('chi_siamo'),
+        'align' => 'left',
+    ],
+    [
+        'title' => 'Servizi',
+        'content' => get_field('servizi'),
+        'align' => 'right',
+    ],
+    [
+        'title' => 'La storia di EO Baussano',
+        'content' => get_field('la_storia_di_eo_baussano'),
+        'align' => 'left',
+    ],
+];
+
+get_template_part(
+    'template-parts/layouts/static-page-sections',
+    null,
+    ['sections' => $sections]
+);
+
+get_template_part('parts/footer');
