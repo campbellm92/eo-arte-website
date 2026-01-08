25d8ca70222ec63441b0723558018cfac0ee4852
Matthew Campbell <m.campbell92@gmail.com>
2026-01-08 14:16:34 +0100

    render pre-footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,27 +1,20 @@
-<footer class="flex flex-col items-center md:flex-row md:justify-between w-full mt-10 p-8">
+<?php
+if (is_front_page() || is_page('contattaci')) {
+    get_template_part('parts/pre-footer');
+}
+?>
 
+
+<footer class="flex flex-col items-center md:flex-row md:justify-between w-full p-8">
     <div class="flex justify-center md:text-left mb-6 md:mb-0">
         <a href="/" class="p-3" id="site-title"><img
                 src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo-eo-arte.svg"
                 alt="EO Arte logo" class="w-20"></a>
     </div>
-
     <a href="https://www.mattdev.it/" class="italic mb-6 md:mb-0 text-xs">Made with ❤️ by Matthew</a>
-
-
     <a href="https://www.arcicuneoasti.com/">
         <?php echo get_svg_markup('arci-asti-cuneo-footer') ?>
     </a>
-
-
-
-
-
-
-
-
-
-    <!-- <a href="https://www.mattdev.it/" class="text-blue italic">Made with ❤️ by Matthew</a> -->
 </footer>
 
 <?php
