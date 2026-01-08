6df69880a1be754743384dde71b02ec982eeab40
Matthew Campbell <m.campbell92@gmail.com>
2026-01-08 14:17:28 +0100

    add initial markup for prefooter

---
+++ parts/pre-footer.php
@@ -0,0 +1,11 @@
+<section class="w-full bg-zinc-100">
+    <div class="max-w-6xl mx-auto px-6 py-8">
+        <div class="grid gap-6 md:grid-cols-3 text-blue text-center md:text-left text-sm">
+            <div>Via Esempio 12, 14100 Asti (AT)</div>
+            <div class="md:text-center">
+                info@eoarte.it | +39 0123 456789
+            </div>
+            <div class="md:text-right">Other info</div>
+        </div>
+    </div>
+</section>
