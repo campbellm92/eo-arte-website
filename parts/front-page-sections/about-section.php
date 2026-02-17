ca1e96ee053a0a2f8bd21f03b9acb9335b7cfb35
Matthew Campbell <m.campbell92@gmail.com>
2026-02-17 17:10:40 +0100

    update static page content

--- parts/front-page-sections/about-section.php
+++ parts/front-page-sections/about-section.php
@@ -2,20 +2,22 @@
 <section class="flex flex-col gap-4 p-8 mt-10 min-h-screen w-full">
     <div class="md:p-5">
         <h1 class="pb-5">CHI SIAMO</h1>
-        <p class="md:w-[65%]">It's 3am, time to create some chaos commence midnight zoomies yet i
-            is
-            not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
-            sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
-            signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
-            the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+        <p class="md:w-[65%]">EO ARTE è un’associazione culturale nata nel 2023 ad Asti all’interno dell’ex Casa
+            Baussano, uno spazio in disuso che abbiamo riattivato nel cuore della città come luogo dedicato all’arte
+            contemporanea, alla ricerca e al lavoro collettivo.</p>
     </div>
     <div class="flex flex-col md:items-end md:p-5">
+        <h1 class="pb-5">STORIA</h1>
+        <p class="md:w-[65%]">Partendo dalla memoria di Eo Baussano, artista e figura della Resistenza, intendiamo
+            l’atto creativo come una forma di responsabilità verso il presente: lo spazio è un laboratorio vivo in cui
+            mostre, progetti espositivi, laboratori e incontri pubblici diventano occasioni di relazione, formazione e
+            produzione culturale.</p>
+    </div>
+    <div class="flex flex-col md:p-5">
         <h1 class="pb-5">SERVIZI</h1>
-        <p class="md:w-[65%]">It's 3am, time to create some chaos commence midnight zoomies yet i
-            is
-            not fat, i is fluffy waffles terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not
-            sorry. Claws in the eye of the beholder go crazy with excitement when plates are clanked together
-            signalling the arrival of cat food my water bowl is clean and freshly replenished, so i'll drink from
-            the toilet for the dog smells bad so sun bathe, so intrigued by the shower.</p>
+        <p class="md:w-[65%]">EO ARTE offre affitto di spazio espositivo e laboratoriale, organizzazione di laboratori e
+            workshop, utilizzo del forno per ceramica e del torchio da stampa A4, servizio di shooting fotografico
+            professionale per opere d’arte e progetti, sostenendo artistə, artigianə e realtà del territorio nella loro
+            ricerca e nella loro visibilità.</p>
     </div>
 </section>
