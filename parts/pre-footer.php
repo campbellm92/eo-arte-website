cd2607289d0fd89ccc812de007d93de52d6e476c
Matthew Campbell <m.campbell92@gmail.com>
2026-02-18 16:32:44 +0100

    add info to prefooter

--- parts/pre-footer.php
+++ parts/pre-footer.php
@@ -1,11 +1,18 @@
 <section class="w-full bg-zinc-100">
     <div class="max-w-6xl mx-auto px-6 py-8">
-        <div class="grid gap-6 md:grid-cols-3 text-blue text-center md:text-left text-sm">
-            <div>Via Esempio 12, 14100 Asti (AT)</div>
-            <div class="md:text-center">
-                info@eoarte.it | +39 0123 456789
+        <div class="grid gap-6 md:grid-cols-3 text-blue text-center md:text-left text-sm items-center">
+            <div>
+                <p>EO ARTE APS</p>
+                <p>via XX settembre, 112 - ASTI</p>
             </div>
-            <div class="md:text-right">Other info</div>
+            <div class="md:text-center">
+                <p>eo.arte.asti@gmail.com</p>
+            </div>
+            <div class="md:text-right">
+                <p>C.F: 92082400059</p>
+                <p>PARTITA IVA: 01761470051</p>
+                <p>IBAN: IT12Y0853010304000000283337</p>
+            </div>
         </div>
     </div>
 </section>
