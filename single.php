f252978e40e9ad1fe87aa7ef8418355a5071ad3b
Matthew Campbell <m.campbell92@gmail.com>
2026-02-20 17:48:17 +0100

    refactor: change layout when client only adds poster/graphic for an event or workshop

--- single.php
+++ single.php
@@ -10,34 +10,54 @@
 
 get_template_part('parts/header');
 
-$has_thumb = has_post_thumbnail();
 ?>
 
 <main class="bg-gray min-h-screen">
     <?php if (have_posts()):
         while (have_posts()):
             the_post(); ?>
-            <!-- hero -->
             <?php
-            $section_class = !$has_thumb ? 'mt-40' : '';
+            $only_has_graphic = (bool) get_post_meta(get_the_ID(), 'only_has_graphic', true);
+            $has_thumb = has_post_thumbnail();
             ?>
-            <section class="<?php echo esc_attr($section_class); ?>">
-                <?php if ($has_thumb): ?>
-                    <?php get_template_part('parts/single-event-or-workshop-page-sections/hero'); ?>
-                <?php endif; ?>
-            </section>
-            <!-- main content -->
-            <section class="m-5 md:m-10">
-                <div class="flex flex-col md:flex-row md:gap-8">
-                    <div class="flex flex-col flex-1">
-                        <?php get_template_part('parts/single-event-or-workshop-page-sections/content'); ?>
-                    </div>
-                    <aside class="md:w-80 shrink-0">
-                        <?php get_template_part('parts/single-event-or-workshop-page-sections/sidebar'); ?>
-                    </aside>
 
-                </div>
-            </section>
+            <?php if ($only_has_graphic): ?>
+
+                <!-- render this if 'solo grafica' is selected in the Wordpress admin for this event -->
+                <section class="mx-6 mt-24">
+                    <div class="flex flex-col md:flex-row md:gap-24">
+                        <?php if ($has_thumb): ?>
+                            <?php the_post_thumbnail('large'); ?>
+                        <?php endif; ?>
+                        <aside class="md:w-80 shrink-0">
+                            <?php get_template_part('parts/single-event-or-workshop-page-sections/sidebar'); ?>
+                        </aside>
+                    </div>
+                </section>
+
+            <?php else: ?>
+                <!-- hero -->
+                <?php
+                $section_class = !$has_thumb ? 'mt-40' : '';
+                ?>
+                <section class="<?php echo esc_attr($section_class); ?>">
+                    <?php if ($has_thumb): ?>
+                        <?php get_template_part('parts/single-event-or-workshop-page-sections/hero'); ?>
+                    <?php endif; ?>
+                </section>
+                <!-- main content -->
+                <section class="m-5 md:m-10">
+                    <div class="flex flex-col md:flex-row md:gap-8">
+                        <div class="flex flex-col flex-1">
+                            <?php get_template_part('parts/single-event-or-workshop-page-sections/content'); ?>
+                        </div>
+                        <aside class="md:w-80 shrink-0">
+                            <?php get_template_part('parts/single-event-or-workshop-page-sections/sidebar'); ?>
+                        </aside>
+
+                    </div>
+                </section>
+            <?php endif; ?>
         <?php endwhile; endif; ?>
 </main>
 
